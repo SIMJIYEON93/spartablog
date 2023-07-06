2a6fbc1db125b2cd034ba6f1f94eb74cf4ee5874
SIMJIYEON93 <930515s@gmail.com>
2023-07-06 16:34:28 +0900

    INIT

--- src/main/java/com/sparta/spartablog/dto/SignupRequestDto.java
+++ src/main/java/com/sparta/spartablog/dto/SignupRequestDto.java
@@ -11,10 +11,10 @@ import lombok.Setter;
 @Setter
 public class SignupRequestDto {
     @NotBlank
-    @Pattern(regexp = "^[a-z0-9]{4,10}$",message = "최소 4자이상 10자 이하여야 합니다.")
+    @Pattern(regexp = "^[a-z0-9]{4,10}$",message = "알파벳 소문자 및 숫자 포함 최소 4자이상 10자 이하여야 합니다.")
     private String username;
     @NotBlank
-    @Pattern(regexp = "^[a-zA-Z0-9]{8,16}$",message = "최소 8자이상 16자 이하여야 합니다.")
+    @Pattern(regexp = "^[a-zA-Z0-9]{8,16}$",message = "알파벳 대소문자 및 숫자 포함 최소 8자이상 16자 이하여야 합니다.")
     private String password;
     @Email
     @NotBlank
